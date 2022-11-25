91d61f04badbf4f45edee3f6dea42c8abb5aede9
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-11-25 11:00:40 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import pyttsx3
+
+listner = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+try:
+    with sr.Microphone() as source:
+        print('listening daddy........')
+        voice = listner.listen(source)
+        command = listner.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            talk(command)
+except:
+    pass
